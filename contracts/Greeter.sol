c6f9b8b6d4e452ef58e92e94727cabe2098294a2
Ilan Pillemer <ilan.pillemer@gmail.com>
2017-09-04 12:20:41 +0100

    get rid of truffle, try populus as test framework
    truffle imposes their version of solc, which is a showstopper for me

---
+++ contracts/Greeter.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0;
+
+contract Mortal {
+    /* Define variable owner of the type address */
+    address owner;
+
+    /* This function is executed at initialization and sets the owner of the contract */
+    function Mortal() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
+
+contract Greeter is Mortal {
+    string public greeting;
+
+    function Greeter() {
+        greeting = 'Hello Testnet World!';
+    }
+
+    function setGreeting(string _greeting) public {
+        greeting = _greeting;
+    }
+
+    function greet() constant returns (string) {
+        return greeting;
+    }
+}
